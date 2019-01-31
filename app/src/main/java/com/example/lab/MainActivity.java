52773316d63f95605dfa0545b75c759d909e73c3
zhouweiming <373518155@qq.com>
2019-01-31 14:37:15 +0800

    框架模板增加了存储权限的申请及其它优化

--- app/src/main/java/com/example/lab/MainActivity.java
+++ app/src/main/java/com/example/lab/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.lab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.security.Permission;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SLog.info("onCreate");
+
+        PermissionUtil.requestStoragePermission(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
     }
 }
