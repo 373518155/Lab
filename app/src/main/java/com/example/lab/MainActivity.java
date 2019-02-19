4668a6d50cef16e6500c59d6caacfbba7f763806
zhouweiming <373518155@qq.com>
2019-02-19 16:12:03 +0800

    优化引入代码

--- app/src/main/java/com/example/lab/MainActivity.java
+++ app/src/main/java/com/example/lab/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.lab;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
-import java.security.Permission;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
