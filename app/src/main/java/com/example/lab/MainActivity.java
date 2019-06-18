325eda212c99e9ac972dc5ad5f15cb5b06b98a01
zhouweiming <373518155@qq.com>
2019-06-18 14:14:49 +0800

    完成了简单的拍照功能

--- app/src/main/java/com/example/lab/MainActivity.java
+++ app/src/main/java/com/example/lab/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lab;
 
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,12 +10,34 @@ import android.widget.FrameLayout;
 
 import com.yanzhenjie.permission.Permission;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * https://developer.android.google.cn/guide/topics/media/camera.html
  */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Camera mCamera;
     CameraPreview mPreview;
+    Camera.PictureCallback pictureCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            String absoluteFilePath = Environment.getExternalStorageDirectory() + "/1/" + System.currentTimeMillis() + ".jpg";
+            SLog.info("absoluteFilePath[%s]", absoluteFilePath);
+            File file = new File(absoluteFilePath);
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         PermissionUtil.requestStoragePermission(this);
 
         findViewById(R.id.btn_get_number_of_cameras).setOnClickListener(this);
-        findViewById(R.id.btn_open_camera).setOnClickListener(this);
+        findViewById(R.id.btn_capture).setOnClickListener(this);
 
         PermissionUtil.actionWithPermission(this, Permission.Group.CAMERA, "使用本APP需要授予", new CommonCallback() {
             @Override
@@ -52,18 +75,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (id == R.id.btn_get_number_of_cameras) {
             int numberOfCameras = Camera.getNumberOfCameras();
             SLog.info("numberOfCameras[%d]", numberOfCameras);
-        } else if (id == R.id.btn_open_camera) {
-            if (mCamera != null) {
-                return;
-            }
-            mCamera = getCameraInstance();
-
-            // 獲取參數
-            Camera.Parameters parameters = mCamera.getParameters();
-
-            // 獲取信息
-            Camera.CameraInfo cameraInfo = null;
-            Camera.getCameraInfo(Camera.CameraInfo.CAMERA_FACING_BACK, cameraInfo);
+        } else if (id == R.id.btn_capture) {
+            mCamera.takePicture(null, null, pictureCallback);
         }
     }
 
